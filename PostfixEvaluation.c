3fff79ffad6a68987e681e35bef02dd3cd02279f
Arjun <arjunk.karjun.arjunk@gmail.com>
2025-09-21 18:28:30 +0530

    experiment 8

---
+++ PostfixEvaluation.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<ctype.h>
+#define MAX 100
+char postfix[MAX];
+char stack[MAX];
+int top=-1;
+
+void push(char c){
+    stack[++top]=c;
+}
+
+int isOperator(char c){
+    return (c=='+'||c=='-'||c=='*'||c=='/');
+}
+
+char pop(){
+        return stack[top--];
+}
+
+int calc(int a,int b,char c){
+    switch(c){
+        case '+':
+                return a+b;
+                break;
+        case '-':
+                return a-b;
+                break;
+        case '*':
+                return a*b;
+                break;
+        case '/':
+                return a/b;
+                break;
+    }
+}
+void eval(){
+    char c;
+    for(int i=0;postfix[i]!='\0';i++){
+        c=postfix[i];
+        if(isdigit(c)){
+            int a = c-'0';
+            push(a);
+        }
+        else if(isOperator(c)){
+            int b=pop();
+            int a=pop();
+            int ans=calc(a,b,c);
+            push(ans);
+        }
+    }
+}
+void main(){
+    printf("Enter the postfix expression: ");
+    scanf("%s",postfix);
+    eval();
+    printf("\n Answer = %d",stack[top]);
+}
